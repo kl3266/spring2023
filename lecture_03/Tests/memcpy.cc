6bb461d5b335772910bf43d9222e5553874397f0
joseemoreira <joseemoreira@DESKTOP-B38EB5F>
2023-01-28 11:22:39 -0500

    added throughput

--- lecture_03/Tests/memcpy.cc
+++ lecture_03/Tests/memcpy.cc
@@ -22,13 +22,17 @@ int main
 	simple::zeroctrs();
 	simple::caches::L1.clear();
 	simple::memcpy(0,0,0);
+
+	double rate = (double)simple::cycles/(double)n;
 	
-	printf("n = %6d : instructions = %6lu, cycles = %6lu, L1 accesses= %6lu, L1 hits = %6lu :",
+	printf("n = %6d : instructions = %6lu, cycles = %6lu, L1 accesses= %6lu, L1 hits = %6lu",
 		n, simple::instructions, simple::cycles, simple::counters::L1::accesses, simple::counters::L1::hits);
+	printf(", cyc/B = %10.2f", rate);
+
 	bool pass = true;
 	for (uint32_t i=0; i<n; i++) if (simple::MEM[i] != simple::MEM[N+i]) pass = false;
-	if (pass) printf("PASS\n");
-	else      printf("FAIL\n");
+	if (pass) printf(" | PASS\n");
+	else      printf(" | FAIL\n");
     }
     
     return 0;
--- lecture_03/Tests/mxv.cc
+++ lecture_03/Tests/mxv.cc
@@ -10,20 +10,22 @@ int main
 {
     printf("L1: %u bytes of capacity, %u sets, %u-way set associative, %u-byte line size\n", 
 	   simple::caches::L1.capacity(), simple::caches::L1.nsets(), simple::caches::L1.nways(), simple::caches::L1.linesize());
-    simple::zeromem();
 
-    const uint32_t M = 32;
-    const uint32_t N = 64;
-
-    const uint32_t Y = 0;
-    const uint32_t X = Y + M*sizeof(double);
-    const uint32_t A = X + N*sizeof(double);
-
-    for (uint32_t i=0; i<M; i++) *((double*)(simple::MEM + Y + i*sizeof(double))) = 0.0;
-    for (uint32_t j=0; j<N; j++) *((double*)(simple::MEM + X + j*sizeof(double))) = (double)j;
-    for (uint32_t i=0; i<M; i++) for (uint32_t j=0; j<N; j++) *((double*)(simple::MEM + A + (i*N+j)*sizeof(double))) = (double)i;
-
+    for (uint32_t m = 2; m <= 64; m *= 2) for (uint32_t n = m; n <= m*2; n *= 2)
     {
+	simple::zeromem();
+
+	const uint32_t M = m;
+	const uint32_t N = n;
+
+	const uint32_t Y = 0;
+	const uint32_t X = Y + M*sizeof(double);
+	const uint32_t A = X + N*sizeof(double);
+
+	for (uint32_t i=0; i<M; i++) *((double*)(simple::MEM + Y + i*sizeof(double))) = 0.0;
+	for (uint32_t j=0; j<N; j++) *((double*)(simple::MEM + X + j*sizeof(double))) = (double)j;
+	for (uint32_t i=0; i<M; i++) for (uint32_t j=0; j<N; j++) *((double*)(simple::MEM + A + (i*N+j)*sizeof(double))) = (double)i;
+
 	simple::GPR[3] = Y;
 	simple::GPR[4] = A;
 	simple::GPR[5] = X;
@@ -34,16 +36,16 @@ int main
 	simple::caches::L1.clear();
 	simple::mxv(0,0,0,0,0);
 	
-	printf("M = %6d, N = %6d : instructions = %6lu, cycles = %6lu, L1 accesses= %6lu, L1 hits = %6lu :",
-		M, N, simple::instructions, simple::cycles, simple::counters::L1::accesses, simple::counters::L1::hits);
+	printf("M = %6d, N = %6d : instructions = %6lu, cycles = %8lu, L1 accesses= %6lu, L1 misses = %6lu :",
+		M, N, simple::instructions, simple::cycles, simple::counters::L1::accesses, simple::counters::L1::misses);
 	bool pass = true;
 	for (uint32_t i=0; i<M; i++)
 	{
 	    double y = *((double*)(simple::MEM + Y + i*sizeof(double)));
-	    printf("\ny[%2d] = %10.0f", i, y);
+	    // printf("\ny[%2d] = %10.0f", i, y);
 	    if (y != ((N*(N-1))/2)*i) { pass = false; }
 	}
-	printf("\n");
+	// printf("\n");
 	if (pass) printf("PASS\n");
 	else      printf("FAIL\n");
     }
