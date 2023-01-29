04cf89869760fe7d04c1c6f162c4f651daae6318
kl3266 <123517123+kl3266@users.noreply.github.com>
2023-01-29 10:44:10 -0500

    TA2 Edit

--- homework_01/byte/Src/memcpy.cc
+++ homework_01/byte/Src/memcpy.cc
@@ -11,17 +11,21 @@ namespace simple
     )
     {
         addi(r7, r3, 0);        // preserve GPR[3] so that we can just return it
-loop:   cmpi(r5, 1);            // n == 0?
+        cmpi(n%2, 0);           // check if n is even or odd
+        beq(even);              // n is even -> branch to even
+        lbz(r6, r4);            // load byte from *src
+        stb(r6, r7);            // store byte to *dest
+        addi(r4, r4, 1);        // src++
+        addi(r7, r7, 1);        // dest++
+        addi(r5, r5, -1);       // n--
+even:   cmpi(r5, 0);            // n == 0?
         beq(end);               // while(n != 0) // if prev compare is equal, branch to end line 22
-        lhz(r6, r4);            // load byte from *src
-        sth(r6, r7);            // store byte to *dest
+        lhz(r6, r4);            // load halfword from *src
+        sth(r6, r7);            // store halfword to *dest
         addi(r4, r4, 2);        // src+=2
         addi(r7, r7, 2);        // dest+=2
         addi(r5, r5, -2);       // n-=2
-        //if n==1 at the end... don't run it every time... odd a little slower
-        lbz(r6, r4);
-        stb(r6, r7);
-        b(loop);                // end while // branch to address loop line 14
+        b(even);                // end while // branch to address loop line 14
 end:    return dest;                //define undef end loop b(loop)? beq(end)? what are these?
     }
 };
